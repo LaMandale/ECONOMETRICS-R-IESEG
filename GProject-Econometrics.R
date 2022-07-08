25161b278cf08c903a653c47403ca3867f77e31a
enzomqs <103044231+enzomqs@users.noreply.github.com>
2022-07-08 14:51:17 +0200

    Update GProject-Econometrics.R

--- GProject-Econometrics.R
+++ GProject-Econometrics.R
@@ -104,13 +104,15 @@ linearHypothesis(model2, c("EXRET_P1=0", "DTB3=0"))
 ###############################################
 # Case 2: GDP Forecasting (Weight: 60%)
 ###############################################
+
 #1.	Start by removing the last 4 observations from the dataset in order to have GDP data until and including 2019Q4. Then, draw a time series graph of GDP. Interpret the series from a visual perspective. Explain whether you think it is stationary or not.
- 
-rv_dataPAGE3[-c(101, 102, 103, 104),,drop=F]
+
+rvdata3 <- rv_dataPAGE3[-c(100, 101, 102, 103, 104), ]
+
 #--------#
 #2.	Conduct an Augmented Dickey-Fuller test and interpret the results.
 library(tseries)
-adf.test(rv_dataPAGE3$GPD)
+adf.test(rvdata3$GPD)
 #--------#
 #3.	If the series is not stationary add the necessary boxes with your answers
 #a
@@ -119,40 +121,45 @@ adf.test(rv_dataPAGE3$GPD)
 #d
 #--------#
 #4.	Show a graph of the ACF (autocorrelation function) and PACF (partial autocorrelation function). 
-acf(rv_dataPAGE3$GPD)
-pacf(rv_dataPAGE3$GPD)
+acf(rvdata3$GPD)
+pacf(rvdata3$GPD)
 #--------#
 #5.	Compute several ARMA models, each time by changing the parameter p and the parameter q, and record the AIC (Akaike information criterion).
 #a.
 
-RGDP <- Delt(rv_dataPAGE3$GPD)
+RGDP <- Delt(rvdata3$GPD)
 
-ARMARGDP <- arima(RGDP, order = c(0,0,0))
-ARMARGDP1 <- arima(RGDP, order = c(0,0,1))
-ARMARGDP2 <- arima(RGDP, order = c(0,0,2))
-ARMARGDP3 <- arima(RGDP, order = c(0,0,3))
-ARMARGDP10 <- arima(RGDP, order = c(1,0,0))
-ARMARGDP4 <- arima(RGDP, order = c(1,0,1))
-ARMARGDP5 <- arima(RGDP, order = c(1,0,2))
-ARMARGDP6 <- arima(RGDP, order = c(1,0,3))
-ARMARGDP11 <- arima(RGDP, order = c(2,0,0))
-ARMARGDP12 <- arima(RGDP, order = c(2,0,1))
-ARMARGDP7 <- arima(RGDP, order = c(2,0,2))
-ARMARGDP8 <- arima(RGDP, order = c(2,0,3))
-ARMARGDP13 <- arima(RGDP, order = c(3,0,0))
-ARMARGDP14 <- arima(RGDP, order = c(3,0,1))
-ARMARGDP15 <- arima(RGDP, order = c(3,0,2))
-ARMARGDP16 <- arima(RGDP, order = c(3,0,3))
+arima(RGDP, order = c(0,0,0))
+arima(RGDP, order = c(0,0,1))
+arima(RGDP, order = c(0,0,2))
+arima(RGDP, order = c(0,0,3))
+arima(RGDP, order = c(1,0,0))
+arima(RGDP, order = c(1,0,1))
+arima(RGDP, order = c(1,0,2))
+arima(RGDP, order = c(1,0,3))
+arima(RGDP, order = c(2,0,0))
+arima(RGDP, order = c(2,0,1))
+arima(RGDP, order = c(2,0,2))
+arima(RGDP, order = c(2,0,3))
+arima(RGDP, order = c(3,0,0))
+arima(RGDP, order = c(3,0,1))
+arima(RGDP, order = c(3,0,2))
+arima(RGDP, order = c(3,0,3))
+
 ###b. The Model that has the lowest AIC is ARMA(0,1) with an AIC equal to -536.18.
 #c.
 #--------#
 #6.	Perform the ARMA regression that you determined in Step 5c. Show the table of results in your report (no need to comment on these results for now).
-ARMARGDP1 <- arima(RGDP, order = c(0,0,1))
+ARMARGDP1 <- arima(RGDP, order = c(1,0,0))
 print(ARMARGDP1)
+
+
 #--------#
 #7.	Using the residuals from the regression in Step 6:
 resid <- residuals(ARMARGDP1)
-plot.ts(resid)
+plot.ts(resid, main="Resid ARMA(1,0)")
+
+
 acf(resid, main="Resid ARMA(0,1)")
 Box.test(resid, lag = 4, type = c("Ljung-Box"))
 Box.test(resid, lag = 8, type = c("Ljung-Box"))
@@ -177,7 +184,7 @@ lines(CIlo, col="blue", lty=2)
 
 #--------#
 #9.	Compute the GDP forecast, and plot in a graph the original GDP series together with its forecast (can be done either in Excel or in R). Comment on this graph. Tell whether the actual European GDP has already bounced back and reached the lost GDP due to the pandemic, according to the forecasted GDP had the pandemic not occurred.
-gdpf <- Delt(rv_dataPAGE3$GPD)
+gdpf <- (rvdata3$GPD)
 armagdpf <- arima(gdpf, order = c(2,0,0))
 fittedgdp <- fitted(armagdpf)
 plot.ts(gdpf)
@@ -185,18 +192,23 @@ lines(fittedgdp, col = "blue")
 
 fgdp <- (predict(armagdpf, se.fit = TRUE, n.ahead = 6))
 
-plot.ts(gdpf, xlim2 = c(300,length(gdpf)+6))
+
+plot.ts(gdpf, ylim = c(2400,3800), xlim = c(0,105))
 lines((fgdp$pred), col="blue")
 CIhi <- fgdp$pred+2*fgdp$se
 CIlo <- fgdp$pred-2*fgdp$se
 lines(CIhi, col="blue", lty=2)
 lines(CIlo, col="blue", lty=2)
 
+lines(rv_dataPAGE3$GPD)
+
 #--------#
 #10.	Write a conclusion to this case (about 300 words) among which:
 #a.	Write a short summary of what you did
 #b.	What do we learn from these results?
 #c.	What do you think about the results?
+
+
 
 
   
